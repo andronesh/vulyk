d69af247ee6caadd926a56ebea32ced328583eb4
Andrii Shalankevych <shalankevych@gmail.com>
2025-01-13 13:44:35 +0200

    Add sidebar and "drones" page

---
+++ nextjs_vulyk/src/app/drones/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<div className="p-4 border-2 border-gray-200 border-solid rounded-lg dark:border-gray-700">
+			list of drones
+		</div>
+	);
+}
