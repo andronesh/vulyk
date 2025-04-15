a4402e38a60e854f8deb2655bcbebd7903d71a4e
Andrii Shalankevych <shalankevych@gmail.com>
2025-04-15 08:57:57 +0300

    Show spinned when models are fetching

--- nextjs_vulyk/src/components/models/ModelsPanel.tsx
+++ nextjs_vulyk/src/components/models/ModelsPanel.tsx
@@ -6,6 +6,7 @@ import ModelsListElement from "./ModelsListElement";
 import ModelDetailsPanel from "./ModelDetailsPanel";
 import { ModelWithOptionsDto } from "@/logic/repo/modelsRepo";
 import ModelCreateButton from "./ModelCreateButton";
+import Spinner from "../common/Spinner";
 
 type Props = {
 	className?: string;
@@ -13,20 +14,28 @@ type Props = {
 
 export default function ModelsPanel(props: Props) {
 	const [selectedModel, setSelectedModel] = useState<ModelWithOptionsDto>();
-	const { data: allModels } = useAllModelsWithOptionsQuery();
+	const { data: allModels, isFetching, isError } = useAllModelsWithOptionsQuery();
 
 	return (
 		<div className="flex space-x-2">
 			<div className={`flex h-fit w-72 flex-col justify-around space-y-2 ${props.className}`}>
 				<ModelCreateButton />
-				{allModels?.map((model) => (
-					<ModelsListElement
-						key={model.id}
-						model={model}
-						onClick={setSelectedModel}
-						isSelected={selectedModel?.id === model.id}
-					/>
-				))}
+				{isError && (
+					<div className="rounded-sm bg-red-700 p-2 text-lg text-white">
+						Не вдалось завантажити список груп з бази данних
+					</div>
+				)}
+				{isFetching && <Spinner />}
+				{!isFetching &&
+					!isError &&
+					allModels?.map((model) => (
+						<ModelsListElement
+							key={model.id}
+							model={model}
+							onClick={setSelectedModel}
+							isSelected={selectedModel?.id === model.id}
+						/>
+					))}
 			</div>
 			{selectedModel && <ModelDetailsPanel model={selectedModel} className="w-72" />}
 		</div>
